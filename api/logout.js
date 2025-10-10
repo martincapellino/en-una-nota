6cce4e45fca15729a1e5ff7381b0d95bd7ef02ff
martincapellino <158485242+martincapellino@users.noreply.github.com>
2025-10-10 18:41:47 -0300

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-	res.setHeader('Set-Cookie', 'spotify_refresh_token=; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=0');
+	res.setHeader('Set-Cookie', 'spotify_refresh_token=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
 	return res.status(200).json({ ok: true });
 };
 
